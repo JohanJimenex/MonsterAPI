e4d0739c6847d1ff71694de42bf692848bbead2e
Johan Jiménez <johan.jimenez.23@gmail.com>
2024-11-28 12:53:54 -0400

    prueba de keyvaul y variables de ambiente

--- MonsterApi/Controllers/MonsterController.cs
+++ MonsterApi/Controllers/MonsterController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MonsterApi.Models;
 using MonsterApi.Services;
+using Microsoft.Extensions.Configuration;
 namespace MonsterApi.Controllers;
 
 
@@ -10,14 +11,30 @@ namespace MonsterApi.Controllers;
 public class MonsterController : ControllerBase {
 
     private readonly MonsterService _monsterService;
+    private readonly IConfiguration _configuration;
 
-    public MonsterController(MonsterService monsterService) {
+    public MonsterController(MonsterService monsterService, IConfiguration configuration) {
         _monsterService = monsterService;
+        _configuration = configuration;
     }
 
     [HttpGet]
     public ActionResult<Monster> Get() {
         return Ok(_monsterService.Get());
+    }
+
+    [HttpGet("prueba")]
+    public string ObtenerValor()
+    {
+        var valor = _configuration["mikey"];
+        return valor;
+    }
+
+     [HttpGet("prueba2")]
+    public string ObtenerValor()
+    {
+        var valor2 = _configuration["miotrokey"];
+        return valor2;
     }
 
     // POST api/monster
